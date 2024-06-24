f7eac7a61385a7b1350a59903fce27d1a7f2f900
DIFIKYT <ragexses@mail.ru>
2024-06-24 12:29:53 +0700

    Commit

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,18 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Min(1)] private int _maxHealth;
+    [SerializeField] private Button _reduceHealthButton;
+    [SerializeField] private Button _restoreHealthButton;
+
+    private int _ñurrentHealth;
+    private bool IsAlive => _ñurrentHealth > 0;
+
+    private void OnEnable()
     {
-        
+        _reduceHealthButton.onClick.AddListener(Reduce);
+        _restoreHealthButton.onClick.AddListener(Restore);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        _reduceHealthButton.onClick.RemoveAllListeners();
+        _restoreHealthButton.onClick.RemoveAllListeners();
+    }
+
+    private void Awake()
+    {
+        _ñurrentHealth = _maxHealth;
+    }
+
+    private void Reduce()
+    {
+        int Amount = 20;
+
+        if (Amount <= 0)
+            return;
+
+        _ñurrentHealth -= Amount;
+
+        if (IsAlive == false)
+            _ñurrentHealth = 0;
+    }
+
+    private void Restore()
+    {
+        int Amount = 20;
+
+        if (Amount <= 0)
+            return;
+
+        _ñurrentHealth = Mathf.Clamp(_ñurrentHealth + Amount, 0, _maxHealth);
     }
 }
